67488f28f48519c0647f5b66f62f098922e6944f
ZamotohinaMaria <zamotokhina.masha@mail.ru>
2022-09-25 23:58:18 +0400

    Написан код для создания папки для картинок и скачивания всех картинок с сайта (пока только 29)

--- save miniature.py
+++ save miniature.py
@@ -5,28 +5,38 @@ import os
 from bs4 import BeautifulSoup
 import requests
 import re
-from re import sub
+from re import I, sub
 from decimal import Decimal
 import io
 from datetime import datetime 
 import pandas as pd
+import shutil
 
 
+print("Текущая деректория:", os.getcwd())
+if os.path.isdir("polar bears")==1:
+     shutil.rmtree('polar bears')
+os.mkdir("polar bears")
+os.chdir("polar bears")
+print("Текущая деректория:", os.getcwd())
 
 URL = 'https://yandex.ru/images/search?from=tabbar&text=polar%20bear'
-print ('URL++======', URL)
 
 html_text = requests.get(URL).text
-#print (html_text)
 
 soup = BeautifulSoup(html_text, 'lxml')
-#print (soup)
 
-img_url = "https:" + str(soup.find('div', class_ = "serp-item__preview").find('a', class_ = "serp-item__link").find('img', class_ = "serp-item__thumb justifier__thumb").get('src'))
-#.find('img', class_ = "serp-item__thumb justifier__thumb")
-print('im=======', img_url)
+img_tags = soup.find_all('img', class_ = "serp-item__thumb justifier__thumb")
 
-img = requests.get(img_url)
-img_op = open('3' + '.jpg', 'wb')
-img_op.write(img.content) 
-img_op.close()
+urls = ["https:"+ img['src'] for img in img_tags]
+print("urls\n", urls)
+i=0
+for url in urls:
+    filename = str(i)+'.jpg'
+    img = requests.get(url)
+    img_op = open(filename, 'wb')
+    img_op.write(img.content) 
+    img_op.close()
+    i+=1
+
+    
