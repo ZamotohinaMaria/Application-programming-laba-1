7553e1e3b797a6b8e1864bfb852f5fda8ef0ac58
ZamotohinaMaria <113538441+ZamotohinaMaria@users.noreply.github.com>
2022-10-05 10:33:27 +0400

    Код переделан под стандарт PEP8

--- save polar bears.py
+++ save polar bears.py
@@ -1,27 +1,19 @@
 #variant №5
-
-import importlib
 import os
 from bs4 import BeautifulSoup
 import requests
-import re
-from re import I, sub
-from decimal import Decimal
-import io
-from datetime import datetime 
-import pandas as pd
 import shutil
-import cv2
 import time
 
-def Image_Search(Count_Find):
-    num_page = 1
+
+def image_search(count_find):
+    num_page = 0
     i = 0
     while True:
-        URL_polar = f'https://yandex.ru/images/search?p={num_page}&from=tabbar&text=polar%20bear&lr=51&rpt=image'
+        url_polar = f'https://yandex.ru/images/search?p={num_page}&from=tabbar&text=polar%20bear&lr=51&rpt=image'
         num_page += 1
         print(num_page)
-        html_text_polar = requests.get(URL_polar).content
+        html_text_polar = requests.get(url_polar).content
         soup_polar = BeautifulSoup(html_text_polar, 'html.parser')
         time.sleep(10)
         
@@ -30,18 +22,18 @@ def Image_Search(Count_Find):
         for link in soup_polar.find_all('img'):
             urls_polar.append("https:" + link.get('src'))
         
-        i = Save_Polar_Bears(urls_polar, Count_Find, i)
+        i = save_images(urls_polar, count_find, i)
 
 
-def Save_Polar_Bears(urls_polar, Count_Find, i):
+def save_images(urls_polar, count_find, i):
     for url in urls_polar:
         if url.find('captcha') != -1 :
             print("\nCAPTCHA ERROR")
-            Finish()
+            finish()
         print(url)
         if url.find('n=13') != -1 :
             try:
-                filename = str(i) + '.jpg'
+                filename = str(i).zfill(4) + '.jpg'
                 img = requests.get(url)
                 img_op = open(filename, 'wb')
                 img_op.write(img.content)
@@ -49,20 +41,22 @@ def Save_Polar_Bears(urls_polar, Count_Find, i):
                 i += 1
             except:
                 print("Error after", i)
-            if i == Count_Find:
-                Finish()
+            if i == count_find:
+                finish()
     return i
 
 
-def Finish():
+def finish():
     print("\nProgramm is finished\n")
     exit(0)
 
-print("Текущая деректория:", os.getcwd())
-if os.path.isdir("polar bears") == 1:
-    shutil.rmtree('polar bears')
-os.mkdir("polar bears")
-os.chdir("polar bears")
-print("Текущая деректория:", os.getcwd())
-Count_Find = 1000
-Image_Search(Count_Find)
+
+if __name__ == '__main__':
+    print("Текущая деректория:", os.getcwd())
+    if os.path.isdir("dataset_polar_bears") == 1:
+        shutil.rmtree('dataset_polar_bears')
+    os.mkdir("dataset_polar_bears")
+    os.chdir("dataset_polar_bears")
+    print("Текущая деректория:", os.getcwd())
+    Count_Find = 30
+    image_search(Count_Find)
