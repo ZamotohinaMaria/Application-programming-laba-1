893d5fcb9418a0ada71c969c84273d3d3e4ae244
ZamotohinaMaria <zamotokhina.masha@mail.ru>
2022-10-20 01:01:08 +0400

    код отформатирован  по стандарту pep8 (использовала для этого pyformat и autopep8, не уверено, что все хорошо отработало в vs code, какие-то глобальные несоответствия убрала в ручную), добавлены докстринги для функций, возможны некоторые добавления после работы в классе(файлы сохранила и перебросила на флешку)

--- _2_class_iterator.py
+++ _2_class_iterator.py
@@ -1,14 +1,30 @@
+import csv
+
+
 class Iterator:
-    def __init__(self, start=0, limit=1100):
-        self.limit = limit
-        self.num = start
+    def __init__(self, file_name, class_name):
+        self.limit = -1
+        self.counter = -1
+        self.file_name = file_name
+        self.class_name = class_name
+        self.rows = []
+        with open(file_name) as file:
+            reader = csv.reader(file)
+            for row in reader:
+                if row[2] == class_name:
+                    self.rows.append(row[0] + ';' + row[2])
+                    self.limit += 1
+
+
     def __iter__(self):
         return self
-    def __next__(self):
-        if self.num < self.limit:
-            num = self.num
-            self.num += 1
-        else:
-            raise StopIteration
-        return num
 
+
+    def __next__(self):
+        if self.counter < self.limit:
+            self.counter += 1
+            return self.rows[self.counter]
+        else:
+            print('None')
+            raise StopIteration
+    
