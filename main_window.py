065c519d367307211f3deafcf9968db54188206a
ZamotohinaMaria <zamotokhina.masha@mail.ru>
2022-10-25 23:22:52 +0400

    создано первое окошко

---
+++ main_window.py
@@ -0,0 +1,15 @@
+import os
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget
+
+if __name__ == '__main__':
+
+    app = QApplication(sys.argv)
+
+    w = QWidget()
+    w.resize(500, 500)
+    w.move(300, 300)
+    w.setWindowTitle('main window')
+    w.show()
+
+    sys.exit(app.exec_())
